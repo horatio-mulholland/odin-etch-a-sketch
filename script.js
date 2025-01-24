39f26db08c128f97662def9dfa65a520abbfb256
horatio-mulholland <horatiomul@gmail.com>
2025-01-24 13:18:28 +0000

    Added NEW button and functionality

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 
 const container = document.querySelector("#container");
 
-for (i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for (j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+function createGrid(size) {
+    for (i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
     }
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("pointerenter", () => {
+            square.style.backgroundColor = "black";
+        });
+    });
 }
 
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-    square.addEventListener("pointerenter", () => {
-        square.style.backgroundColor = "black";
-    });
+createGrid(16);
+
+const newBtn = document.querySelector("#new");
+newBtn.addEventListener("click", () => {
+    num = +prompt("Grid size (1 - 100)?");
+    if (!Number.isInteger(num) || num <= 0) {
+        alert("Enter a valid number");
+    } else if (num > 100) {
+        alert("Must be between 1 and 100.");
+    } else {
+        container.replaceChildren();
+        createGrid(num);
+    }
 });
